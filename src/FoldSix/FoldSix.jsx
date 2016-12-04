d8d252aea511e77c940cda405d919f5c2f9da90a
Eric Casey <ericcasey509@gmail.com>
2016-12-03 22:14:20 -0500

    sat

--- src/FoldSix/FoldSix.jsx
+++ src/FoldSix/FoldSix.jsx
@@ -5,7 +5,9 @@ class FoldSix extends Component {
   render() {
     return (
       <div className="FoldSix" id="FoldSix" onClick={this.props.modeChanger} >
-      <h3>Check out my photo-blog</h3>
+        <div className="content">
+          <h3>Check out my photo-blog</h3>
+        </div>
       </div>
     );
   }
--- src/TopFold/TopFold.jsx
+++ src/TopFold/TopFold.jsx
@@ -7,10 +7,10 @@ class TopFold extends Component {
       <div className="TopFold" id="TopFold">
       <div className="anchor1"></div>
         <div className="content">
-          <h3>Thanks for visiting.</h3>
-          <h3>-- Click something or scroll, you don't have all day. --</h3>
-          <h3>v-- the good stuff is down there --v</h3>
-
+          <h3>Hi there, welcome to my website.</h3>
+          <h3>Below you'll find most of my Web Development projects & contact info.</h3>
+          <h3>Thanks for visting</h3>
+          <h3>-Eric</h3>
         </div>
       </div>
     );
